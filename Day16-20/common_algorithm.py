7a94e1fb890fabd07f1de259c6f959141b60b80a
Asunqingwen <sqw123az@sina.com>
2019-05-21 17:20:09 +0800

    update the code of day16-20

---
+++ Day16-20/common_algorithm.py
@@ -0,0 +1,49 @@
+# 穷举法：百钱百鸡和五人分鱼
+# 用100元买100只鸡 问公鸡/母鸡/小鸡各多少只
+for x in range(20):
+	for y in range(33):
+		z = 100 - x - y
+		if 5 * x + 3 * y + z // 3 == 100 and z % 3 == 0:
+			print(x, y, z)
+'''
+A、B、C、D、E五人在某天夜里合伙捕鱼 最后疲惫不堪各自睡觉
+第二天A第一个醒来 他将鱼分为5份 扔掉多余的1条 拿走自己的一份
+B第二个醒来 也将鱼分为5份 扔掉多余的1条 拿走自己的一份
+然后C、D、E依次醒来也按同样的方式分鱼 问他们至少捕了多少条鱼
+'''
+fish = 1
+while True:
+	total = fish
+	enought = True
+	for _ in range(5):
+		if (total-1)%5 == 0:
+			total = (total-1)//5*4
+		else:
+			enough = False
+			break
+	if enough:
+		print(fish)
+		break
+	fish += 1
+
+#贪婪法：在对问题求解时，总是做出在当前看来是最好的选择，不追求最优解，快速找到满意解
+'''
+假设小偷有一个背包，最多能装20公斤赃物，他闯入一户人家，发现如下表所示的物品。很显然，他不能把所有物品都装进背包，所以必须确定拿走哪些物品，留下哪些物品
+名称	价格（美元）	重量（kg）
+电脑	200	                20
+收音机	20	                4
+钟	    175	                10
+花瓶	50	                2
+书	    10	                1
+油画	90	                9
+'''
+class Thing(object):
+	'''物品'''
+	def __init__(self,name,price,weight):
+		self.name = name
+		self.price = price
+		self.weight = weight
+	@property
+	def value(self):
+		'''价格重量比'''
+		return self.price / self.weight
